398f62020a5f0e77a569dc9b76c1f45b3ca0bf6d
ninibaramidze <166219192+ninibaramidze@users.noreply.github.com>
2024-05-25 17:54:14 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+let name = "nini"
+let hobby = "eating chocolate"
+let asaki = 2009
+let awmyo = 2024
+console.log (my name is + name)
+console.log (my hobby is + hobby)
+console.log (my age is + awmyo - + asaki)
